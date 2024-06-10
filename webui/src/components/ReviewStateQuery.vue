3e5e66a5180fa325b85f8bb5250266fc59066929
Zhao KK <kikakkz@hotmail.com>
2024-06-10 16:52:51 +0800

    Support get review state through CheCko

--- webui/src/components/ReviewStateQuery.vue
+++ webui/src/components/ReviewStateQuery.vue
@@ -18,9 +18,10 @@ const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
 
 const ready = (): boolean => {
-  return reviewApp.value?.length > 0 && targetChain.value?.length > 0
+  return reviewApp.value?.length > 0 /* && targetChain.value?.length > 0 */
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getReviewState = () => {
   const { /* result, */ refetch /*, fetchMore */, onResult /*, onError */ } = provideApolloClient(apolloClient)(() => useQuery(gql`
     query getReviewState {
@@ -60,19 +61,65 @@ const getReviewState = () => {
   })
 }
 
+const getReviewStateThroughCheCko = () => {
+  const query = gql`
+    query getReviewState {
+      contentApprovedThreshold
+      contentRejectedThreshold
+      assetApprovedThreshold
+      assetRejectedThreshold
+      reviewerApprovedThreshold
+      reviewerRejectedThreshold
+      activityApprovedThreshold
+      activityRejectedThreshold
+      reviewerNumber
+    }`
+
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      applicationId: reviewApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {},
+        operationName: 'getReviewState'
+      }
+    }
+  }).then((result) => {
+    console.log(result)
+    const ret = (result as Record<string, number>)
+    review.contentApprovedThreshold = ret.contentApprovedThreshold
+    review.contentRejectedThreshold = ret.contentRejectedThreshold
+    review.assetApprovedThreshold = ret.assetApprovedThreshold
+    review.assetRejectedThreshold = ret.assetRejectedThreshold
+    review.reviewerApprovedThreshold = ret.reviewerApprovedThreshold
+    review.reviewerRejectedThreshold = ret.reviewerRejectedThreshold
+    review.activityApprovedThreshold = ret.activityApprovedThreshold
+    review.activityRejectedThreshold = ret.activityRejectedThreshold
+    review.reviewerNumber = ret.reviewerNumber
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
+watch(blockHeight, () => {
+  if (!ready()) return
+  getReviewStateThroughCheCko()
+})
+
 watch(targetChain, () => {
   if (!ready()) return
-  getReviewState()
+  getReviewStateThroughCheCko()
 })
 
 watch(reviewApp, () => {
   if (!ready()) return
-  getReviewState()
+  getReviewStateThroughCheCko()
 })
 
 onMounted(() => {
   if (!ready()) return
-  getReviewState()
+  getReviewStateThroughCheCko()
 })
 
 </script>
