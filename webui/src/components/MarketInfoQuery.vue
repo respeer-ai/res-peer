f013a35e3cb3072a8f1595a793238b3d235f81bd
Zhao KK <kikakkz@hotmail.com>
2024-06-12 14:00:40 +0800

    Support mint nft through CheCko

--- webui/src/components/MarketInfoQuery.vue
+++ webui/src/components/MarketInfoQuery.vue
@@ -10,6 +10,7 @@ import { ApolloClient } from '@apollo/client/core'
 import { useApplicationStore } from 'src/stores/application'
 import { targetChain } from 'src/stores/chain'
 import { graphqlResult } from 'src/utils'
+import { useSettingStore } from 'src/stores/setting'
 
 const user = useUserStore()
 const account = computed(() => user.account)
@@ -19,29 +20,47 @@ const block = useBlockStore()
 const blockHeight = computed(() => block.blockHeight)
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
+const setting = useSettingStore()
+const cheCkoConnect = computed(() => setting.cheCkoConnect)
 
 const ready = (): boolean => {
-  return account.value?.length > 0 && marketApp.value?.length > 0 && targetChain.value?.length > 0
+  return account.value?.length > 0 && marketApp.value?.length > 0 && (cheCkoConnect.value || targetChain.value?.length > 0)
 }
 
 watch(targetChain, () => {
   if (!ready()) return
-  getMarketInfo()
+  if (cheCkoConnect.value) {
+    getMarketInfoThroughCheCko()
+  } else {
+    getMarketInfo()
+  }
 })
 
 watch(marketApp, () => {
   if (!ready()) return
-  getMarketInfo()
+  if (cheCkoConnect.value) {
+    getMarketInfoThroughCheCko()
+  } else {
+    getMarketInfo()
+  }
 })
 
 watch(account, () => {
   if (!ready()) return
-  getMarketInfo()
+  if (cheCkoConnect.value) {
+    getMarketInfoThroughCheCko()
+  } else {
+    getMarketInfo()
+  }
 })
 
 watch(blockHeight, () => {
   if (!ready()) return
-  getMarketInfo()
+  if (cheCkoConnect.value) {
+    getMarketInfoThroughCheCko()
+  } else {
+    getMarketInfo()
+  }
 })
 
 const collection = useCollectionStore()
@@ -113,9 +132,79 @@ const getMarketInfo = () => {
   })
 }
 
+const getMarketInfoThroughCheCko = () => {
+  const query = (() => {
+    if (account.value) {
+      return gql`
+        query getMarketInfo($account: String!) {
+          collections {
+            keys
+          }
+          creditsPerLinera
+          tradeFeePercent
+          maxCreditsPercent
+          assets {
+            entry(key: $account) {
+              value
+            }
+          }
+          avatars {
+            entry(key: $account) {
+              value
+            }
+          }
+        }`
+    }
+    return gql`
+      query getMarketInfo {
+        collections {
+          keys
+        }
+        creditsPerLinera
+        tradeFeePercent
+        maxCreditsPercent
+      }`
+  })()
+
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      applicationId: marketApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {
+          account: `${account.value}`
+        },
+        operationName: 'getMarketInfo'
+      }
+    }
+  }).then((result) => {
+    const collections = graphqlResult.keyValue(result, 'collections')
+    collection.collectionsKeys = graphqlResult.keys(collections) as Array<number>
+    collection.creditsPerLinera = graphqlResult.keyValue(result, 'creditsPerLinera') as string
+    collection.maxCreditsPercent = graphqlResult.keyValue(result, 'maxCreditsPercent') as number
+    collection.tradeFeePercent = graphqlResult.keyValue(result, 'maxCreditsPercent') as number
+    const _assets = graphqlResult.keyValue(result, 'assets')
+    const assets = graphqlResult.entryValue(_assets) as Record<number, Array<number>>
+    if (assets) {
+      Object.keys(assets).forEach((key, index) => {
+        collection.assets.set(parseInt(key), Object.values(assets)[index])
+      })
+    }
+    const _avatar = graphqlResult.keyValue(result, 'avatars')
+    collection.avatars.set(account.value, graphqlResult.entryValue(_avatar) as Array<number>)
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
 onMounted(() => {
   if (!ready()) return
-  getMarketInfo()
+  if (cheCkoConnect.value) {
+    getMarketInfoThroughCheCko()
+  } else {
+    getMarketInfo()
+  }
 })
 
 </script>
--- webui/src/components/MintNFT.vue
+++ webui/src/components/MintNFT.vue
@@ -33,13 +33,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { getClientOptions } from 'src/apollo'
 import { ApolloClient } from '@apollo/client/core'
 import { provideApolloClient, useMutation } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { useCollectionStore } from 'src/stores/collection'
 import { targetChain } from 'src/stores/chain'
+import { useSettingStore } from 'src/stores/setting'
+import { useApplicationStore } from 'src/stores/application'
 
 const editing = ref(false)
 const uriIndex = ref(-1)
@@ -48,21 +50,15 @@ const ownPrice = ref(false)
 const name = ref('')
 const collectionId = ref(-1)
 const collection = useCollectionStore()
+const setting = useSettingStore()
+const cheCkoConnect = computed(() => setting.cheCkoConnect)
+const application = useApplicationStore()
+const marketApp = computed(() => application.marketApp)
 
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
 
-const onMintlick = async () => {
-  if (uriIndex.value < 0) {
-    return
-  }
-  if (collectionId.value < 0) {
-    return
-  }
-  if (!name.value.length) {
-    return
-  }
-
+const mintNft = async () => {
   const { mutate, onDone, onError } = provideApolloClient(apolloClient)(() => useMutation(gql`
     mutation mintNft ($collectionId: Int!, $uriIndex: Int!, $price: String, $name: String!) {
       mintNft(collectionId: $collectionId, uriIndex: $uriIndex, price: $price, name: $name)
@@ -85,6 +81,53 @@ const onMintlick = async () => {
     endpoint: 'market',
     chainId: targetChain.value
   })
+}
+
+const mintNftThroughCheCko = () => {
+  const query = gql`
+    mutation mintNft ($collectionId: Int!, $uriIndex: Int!, $price: String, $name: String!) {
+      mintNft(collectionId: $collectionId, uriIndex: $uriIndex, price: $price, name: $name)
+    }`
+  window.linera.request({
+    method: 'linera_graphqlMutation',
+    params: {
+      applicationId: marketApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {
+          collectionId: parseInt(collectionId.value.toString()),
+          uriIndex: parseInt(uriIndex.value.toString()),
+          price: ownPrice.value ? price.value : undefined,
+          name: name.value
+        },
+        operationName: 'mintNft'
+      }
+    }
+  }).then(() => {
+    editing.value = !editing.value
+    uriIndex.value = 0
+    collection.mutateKeys.push(parseInt(collectionId.value.toString()))
+  }).catch((e) => {
+    uriIndex.value = 0
+    console.log(e)
+  })
+}
+
+const onMintlick = () => {
+  if (uriIndex.value < 0) {
+    return
+  }
+  if (collectionId.value < 0) {
+    return
+  }
+  if (!name.value.length) {
+    return
+  }
+  if (cheCkoConnect.value) {
+    mintNftThroughCheCko()
+  } else {
+    void mintNft()
+  }
 }
 
 </script>
