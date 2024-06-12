cbdeca2adc7f0a98ffa39f60889650680bfbefd9
Zhao KK <kikakkz@hotmail.com>
2024-06-12 10:29:25 +0800

    Support query with checko

--- webui/src/components/ActivitiesKeysQuery.vue
+++ webui/src/components/ActivitiesKeysQuery.vue
@@ -9,17 +9,20 @@ import { targetChain } from 'src/stores/chain'
 import { useApplicationStore } from 'src/stores/application'
 import { useActivityStore } from 'src/stores/activity'
 import { graphqlResult } from 'src/utils'
+import { useSettingStore } from 'src/stores/setting'
 
 const block = useBlockStore()
 const blockHeight = computed(() => block.blockHeight)
 const activity = useActivityStore()
 const application = useApplicationStore()
 const activityApp = computed(() => application.activityApp)
+const setting = useSettingStore()
+const cheCkoConnect = computed(() => setting.cheCkoConnect)
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
 
 const ready = () => {
-  return targetChain.value?.length > 0 && activityApp.value?.length > 0
+  return (cheCkoConnect.value || targetChain.value?.length > 0) && activityApp.value?.length > 0
 }
 
 const getActivitiesKeys = () => {
@@ -43,19 +46,56 @@ const getActivitiesKeys = () => {
   })
 }
 
+const getActivitiesKeysThroughCheCko = () => {
+  const query = gql`
+    query getActivitiesKeys {
+      activities {
+        keys
+      }
+    }`
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      applicationId: activityApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {},
+        operationName: 'getActivitiesKeys'
+      }
+    }
+  }).then((result) => {
+    const activities = graphqlResult.keyValue(result, 'activities')
+    activity.activitiesKeys = graphqlResult.keys(activities) as Array<number>
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
 watch(blockHeight, () => {
   if (!ready()) return
-  getActivitiesKeys()
+  if (cheCkoConnect.value) {
+    getActivitiesKeysThroughCheCko()
+  } else {
+    getActivitiesKeys()
+  }
 })
 
 watch(activityApp, () => {
   if (!ready()) return
-  getActivitiesKeys()
+  if (cheCkoConnect.value) {
+    getActivitiesKeysThroughCheCko()
+  } else {
+    getActivitiesKeys()
+  }
 })
 
 onMounted(() => {
   if (!ready()) return
-  getActivitiesKeys()
+  if (cheCkoConnect.value) {
+    getActivitiesKeysThroughCheCko()
+  } else {
+    getActivitiesKeys()
+  }
 })
 
 </script>
--- webui/src/components/ActivitiesQuery.vue
+++ webui/src/components/ActivitiesQuery.vue
@@ -8,14 +8,20 @@ import { useBlockStore } from 'src/stores/block'
 import { Activity, useActivityStore } from 'src/stores/activity'
 import { targetChain } from 'src/stores/chain'
 import { graphqlResult } from 'src/utils'
+import { useSettingStore } from 'src/stores/setting'
+import { useApplicationStore } from 'src/stores/application'
 
 const activity = useActivityStore()
 const activitiesKeys = computed(() => activity.activitiesKeys)
 const activities = computed(() => activity.activities)
+const application = useApplicationStore()
+const activityApp = computed(() => application.activityApp)
 const contentIndex = ref(-1)
 const activityKey = computed(() => contentIndex.value >= 0 ? activitiesKeys.value[contentIndex.value] : undefined)
 const block = useBlockStore()
 const blockHeight = computed(() => block.blockHeight)
+const setting = useSettingStore()
+const cheCkoConnect = computed(() => setting.cheCkoConnect)
 
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
@@ -92,13 +98,97 @@ const getActivities = (activityKey: number, done?: () => void) => {
   })
 }
 
+const getActivitiesThroughCheCko = (activityKey: number, done?: () => void) => {
+  const query = gql`
+    query getActivities($activityKey: Int!) {
+      activities {
+        entry(key: $activityKey) {
+          value {
+            id
+            title
+            slogan
+            banner
+            posters
+            introduction
+            host
+            hostResume
+            createdAt
+            activityType
+            votable
+            voteType
+            objectType
+            objectCandidates
+            condition {
+              classes
+              minWords
+              maxWords
+            }
+            sponsors
+            prizeConfigs {
+              place
+              medal
+              title
+              rewardAmount
+            }
+            announcements
+            prizeAnnouncement
+            voterRewardPercent
+            votePowers
+            voters
+            budgetAmount
+            joinType
+            location
+            comments
+            registers
+            registerStartAt
+            registerEndAt
+            voteStartAt
+            voteEndAt
+            participantors
+            winners {
+              place
+              objectId
+            }
+            finalized
+          }
+        }
+      }
+    }`
+
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      applicationId: activityApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {
+          activityKey
+        },
+        operationName: 'getActivitiesKeys'
+      }
+    }
+  }).then((result) => {
+    const _activities = graphqlResult.data(result, 'activities')
+    activities.value.set(Number(activityKey), graphqlResult.entryValue(_activities) as Activity)
+    done?.()
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
 watch(activityKey, () => {
   if (!activityKey.value) {
     return
   }
-  getActivities(activityKey.value, () => {
-    contentIndex.value++
-  })
+  if (cheCkoConnect.value) {
+    getActivitiesThroughCheCko(activityKey.value, () => {
+      contentIndex.value++
+    })
+  } else {
+    getActivities(activityKey.value, () => {
+      contentIndex.value++
+    })
+  }
 })
 
 watch(activitiesKeys, () => {
