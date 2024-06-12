9850751b670479ca721858b82ec6a8b86c4ff698
Zhao KK <kikakkz@hotmail.com>
2024-06-12 12:16:04 +0800

    Support get balance through CheCko

--- webui/src/components/CreditQuery.vue
+++ webui/src/components/CreditQuery.vue
@@ -9,6 +9,7 @@ import { getClientOptions } from 'src/apollo'
 import { useApplicationStore } from 'src/stores/application'
 import { targetChain } from 'src/stores/chain'
 import { graphqlResult } from 'src/utils'
+import { useSettingStore } from 'src/stores/setting'
 
 const user = useUserStore()
 const block = useBlockStore()
@@ -18,13 +19,15 @@ const application = useApplicationStore()
 const creditApp = computed(() => application.creditApp)
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
+const setting = useSettingStore()
+const cheCkoConnect = computed(() => setting.cheCkoConnect)
 
 const ready = () => {
-  return account.value?.length && creditApp.value?.length && targetChain.value?.length
+  return account.value?.length && creditApp.value?.length && (cheCkoConnect.value || targetChain.value?.length)
 }
 
 const getBalance = () => {
-  const { /* result, */ refetch, /* fetchMore, */ onResult /*, onError */ } = provideApolloClient(apolloClient)(() => useQuery(gql`
+  const { /* result, refetch, fetchMore, */ onResult /*, onError */ } = provideApolloClient(apolloClient)(() => useQuery(gql`
     query getBalance($owner: String!) {
       spendables {
         entry(key: $owner) {
@@ -57,43 +60,94 @@ const getBalance = () => {
     const balances = graphqlResult.data(res, 'balances')
     user.amounts = graphqlResult.entryValueKeyValue(balances, 'amounts') as Array<AgeAmount>
   })
+}
 
-  watch(blockHeight, () => {
-    if (!ready()) {
-      return
-    }
-    void refetch({
-      owner: account.value,
-      endpoint: 'credit',
-      chainId: targetChain.value
-    })
-  })
+const getBalanceThroughCheCko = () => {
+  const query = gql`
+    query getBalance($owner: String!) {
+      spendables {
+        entry(key: $owner) {
+          value
+        }
+      }
+      balances {
+        entry(key: $owner) {
+          value {
+            amounts {
+              amount
+              expired
+            }
+          }
+        }
+      }
+    }`
 
-  watch(account, () => {
-    if (!ready()) {
-      return
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      applicationId: creditApp.value,
+      query: {
+        query: query.loc?.source?.body,
+        variables: {
+          owner: account.value
+        },
+        operationName: 'getBalance'
+      }
     }
-    void refetch({
-      owner: account.value,
-      endpoint: 'credit',
-      chainId: targetChain.value
-    })
+  }).then((result) => {
+    console.log('Credits', result)
+    const spendables = graphqlResult.keyValue(result, 'spendables')
+    user.spendable = graphqlResult.entryValue(spendables) as string
+    const balances = graphqlResult.data(result, 'balances')
+    user.amounts = graphqlResult.entryValueKeyValue(balances, 'amounts') as Array<AgeAmount>
+  }).catch((e) => {
+    console.log(e)
   })
 }
 
+watch(blockHeight, () => {
+  if (!ready()) return
+  if (cheCkoConnect.value) {
+    getBalanceThroughCheCko()
+  } else {
+    getBalance()
+  }
+})
+
+watch(account, () => {
+  if (!ready()) return
+  if (cheCkoConnect.value) {
+    getBalanceThroughCheCko()
+  } else {
+    getBalance()
+  }
+})
+
 watch(targetChain, () => {
   if (!ready()) return
-  getBalance()
+  if (cheCkoConnect.value) {
+    getBalanceThroughCheCko()
+  } else {
+    getBalance()
+  }
 })
 
 watch(creditApp, () => {
   if (!ready()) return
-  getBalance()
+  if (cheCkoConnect.value) {
+    getBalanceThroughCheCko()
+  } else {
+    getBalance()
+  }
 })
 
 onMounted(() => {
   if (!ready()) return
-  getBalance()
+  if (cheCkoConnect.value) {
+    getBalanceThroughCheCko()
+  } else {
+    getBalance()
+  }
 })
 
 </script>
