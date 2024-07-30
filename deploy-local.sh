797b39f1654c315f7f78198955ac803f4f3c721d
Zhao KK <kikakkz@hotmail.com>
2024-07-30 09:48:09 +0000

    Support deploy copilot

--- deploy-local.sh
+++ deploy-local.sh
@@ -93,6 +93,13 @@ print $'\U01f499' $LIGHTGREEN " Activity application deployed"
 echo -e "    Bytecode ID:    $BLUE$activity_bid$NC"
 echo -e "    Application ID: $BLUE$activity_appid$NC"
 
+print $'\U01F4AB' $YELLOW " Deploying Copilot application ..."
+copilot_bid=`linera --with-wallet 1 publish-bytecode ./target/wasm32-unknown-unknown/release/copilot_{contract,service}.wasm`
+copilot_appid=`linera --with-wallet 1 create-application $copilot_bid --json-parameters "{\"review_app_id\":\"$review_appid\",\"foundation_app_id\":\"$foundation_appid\",\"feed_app_id\":\"$feed_appid\"}" --required-application-ids $review_appid --required-application-ids $foundation_appid --required-application-ids $feed_appid`
+print $'\U01f499' $LIGHTGREEN " Copilot application deployed"
+echo -e "    Bytecode ID:    $BLUE$copilot_bid$NC"
+echo -e "    Application ID: $BLUE$copilot_appid$NC"
+
 app_deploy_chain=`linera --with-wallet 1 wallet show | grep "Public Key" | awk '{print $2}'`
 app_deploy_owner=`linera --with-wallet 1 wallet show | grep "Owner" | awk '{print $4}'`
 
@@ -102,6 +109,7 @@ sed -i "s/marketApp =.*/marketApp = '$market_appid',/g" webui/src/const/index.ts
 sed -i "s/reviewApp =.*/reviewApp = '$review_appid',/g" webui/src/const/index.ts
 sed -i "s/foundationApp =.*/foundationApp = '$foundation_appid'/g" webui/src/const/index.ts
 sed -i "s/activityApp =.*/activityApp = '$activity_appid',/g" webui/src/const/index.ts
+sed -i "s/copilotApp =.*/copilotApp = '$copilot_appid',/g" webui/src/const/index.ts
 sed -i "s/export const appDeployChain =.*/export const appDeployChain = '$app_deploy_chain'/g" webui/src/const/index.ts
 sed -i "s/export const appDeployOwner =.*/export const appDeployOwner = '$app_deploy_owner'/g" webui/src/const/index.ts
 
