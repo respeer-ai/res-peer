395b5858e951fc5f15e5d7c859eaba26ac5b08e7
Zhao KK <kikakkz@hotmail.com>
2024-08-03 16:37:57 +0800

    Put logo to center

--- webui/src/components/header/MainHeader.vue
+++ webui/src/components/header/MainHeader.vue
@@ -7,11 +7,11 @@
         rounded
         outlined
         placeholder='Title / Content / Comment'
-        :style='{width: "320px"}'
+        :style='{width: "400px"}'
         color='transparent'
       >
         <template #prepend>
-          <q-icon name='bi-filter-left' size='24px' />
+          <q-icon name='bi-filter-left' size='24px' color='red-4' />
         </template>
       </q-input>
     </div>
@@ -22,123 +22,126 @@
       @click='onLogoClick'
     />
     <q-space />
-    <div class='header-icon'>
-      <q-icon
-        name='bi-layout-wtf' size='24px' :color='tab == "store" ? "red-6" : "black"' class='cursor-pointer'
-        @click='onNFTMarketClick'
-      />
-      <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
-        NFT Market Place
-      </q-tooltip>
-    </div>
-    <div class='header-icon'>
-      <q-icon
-        name='bi-columns-gap' size='24px' :color='tab == "activity" ? "red-6" : "black"' class='cursor-pointer'
-        @click='onActivityClick'
-      />
-      <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
-        Activity Center
-      </q-tooltip>
-    </div>
-    <q-btn
-      flat rounded class='bg-red-2'
-      @click='onLoginClick'
-      :style='{marginLeft: "16px"}'
-    >
-      <q-menu
-        v-if='account?.length'
-        :style='{padding: "24px"}'
-        anchor='bottom right'
-        self='top right'
+    <div class='row' :style='{width: "480px"}'>
+      <q-space />
+      <div class='header-icon'>
+        <q-icon
+          name='bi-layout-wtf' size='24px' :color='tab == "store" ? "red-6" : "black"' class='cursor-pointer'
+          @click='onNFTMarketClick'
+        />
+        <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
+          NFT Market Place
+        </q-tooltip>
+      </div>
+      <div class='header-icon'>
+        <q-icon
+          name='bi-columns-gap' size='24px' :color='tab == "activity" ? "red-6" : "black"' class='cursor-pointer'
+          @click='onActivityClick'
+        />
+        <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
+          Activity Center
+        </q-tooltip>
+      </div>
+      <q-btn
+        flat rounded class='bg-red-2'
+        @click='onLoginClick'
+        :style='{marginLeft: "16px"}'
       >
-        <q-card flat>
-          <div class='text-brown-10 row flex justify-center items-center' :style='{margin: "24px 0 0 0", lineHeight: "48px"}'>
-            <q-space />
-            <div class='text-bold' :style='{fontSize: "36px", marginLeft: "8px"}'>
-              {{ Number(accountBalance).toFixed(4) }}
-            </div>
-            <q-space />
-          </div>
-          <div class='row' :style='{margin: "8px 0 48px 0", lineHeight: "24px"}'>
-            <q-space />
-            <q-img
-              src='https://avatars.githubusercontent.com/u/107513858?s=48&v=4'
-              width='24px'
-              height='24px'
-            />
-            <div class='text-brown-6' :style='{fontSize: "24px", marginLeft: "8px"}'>
-              TLINERA
-            </div>
-            <q-space />
-          </div>
-          <q-separator :style='{margin: "8px 0"}' />
-          <div class='row flex justify-center items-center'>
-            <div :style='{width: "48px"}'>
-              <q-icon name='person' size='24px' class='text-red-6' />
-            </div>
-            <div>
-              <div class='text-grey-6'>
-                Address
-              </div>
-              <div class='row'>
-                <div class='text-bold'>
-                  {{ shortid.shortId(account, 16) }}
-                </div>
-                <div :style='{marginLeft: "8px"}' class='cursor-pointer'>
-                  <q-icon name='content_copy' size='16px' class='text-grey-6' />
-                </div>
-              </div>
-              <div class='text-grey-6'>
+        <q-menu
+          v-if='account?.length'
+          :style='{padding: "24px"}'
+          anchor='bottom right'
+          self='top right'
+        >
+          <q-card flat>
+            <div class='text-brown-10 row flex justify-center items-center' :style='{margin: "24px 0 0 0", lineHeight: "48px"}'>
+              <q-space />
+              <div class='text-bold' :style='{fontSize: "36px", marginLeft: "8px"}'>
                 {{ Number(accountBalance).toFixed(4) }}
               </div>
+              <q-space />
             </div>
-          </div>
-          <q-separator :style='{margin: "8px 0"}' />
-          <div class='row flex justify-center items-center'>
-            <div :style='{width: "48px"}'>
-              <q-icon name='link' size='24px' class='text-red-6' />
-            </div>
-            <div>
-              <div class='text-grey-6'>
-                Microchain
+            <div class='row' :style='{margin: "8px 0 48px 0", lineHeight: "24px"}'>
+              <q-space />
+              <q-img
+                src='https://avatars.githubusercontent.com/u/107513858?s=48&v=4'
+                width='24px'
+                height='24px'
+              />
+              <div class='text-brown-6' :style='{fontSize: "24px", marginLeft: "8px"}'>
+                TLINERA
               </div>
-              <div class='row'>
-                <div class='text-bold'>
-                  {{ shortid.shortId(chainId, 16) }}
+              <q-space />
+            </div>
+            <q-separator :style='{margin: "8px 0"}' />
+            <div class='row flex justify-center items-center'>
+              <div :style='{width: "48px"}'>
+                <q-icon name='person' size='24px' class='text-red-6' />
+              </div>
+              <div>
+                <div class='text-grey-6'>
+                  Address
                 </div>
-                <div :style='{marginLeft: "8px"}' class='cursor-pointer'>
-                  <q-icon name='content_copy' size='16px' class='text-grey-6' />
+                <div class='row'>
+                  <div class='text-bold'>
+                    {{ shortid.shortId(account, 16) }}
+                  </div>
+                  <div :style='{marginLeft: "8px"}' class='cursor-pointer'>
+                    <q-icon name='content_copy' size='16px' class='text-grey-6' />
+                  </div>
+                </div>
+                <div class='text-grey-6'>
+                  {{ Number(accountBalance).toFixed(4) }}
                 </div>
               </div>
-              <div class='text-grey-6'>
-                {{ Number(chainBalance).toFixed(4) }}
+            </div>
+            <q-separator :style='{margin: "8px 0"}' />
+            <div class='row flex justify-center items-center'>
+              <div :style='{width: "48px"}'>
+                <q-icon name='link' size='24px' class='text-red-6' />
+              </div>
+              <div>
+                <div class='text-grey-6'>
+                  Microchain
+                </div>
+                <div class='row'>
+                  <div class='text-bold'>
+                    {{ shortid.shortId(chainId, 16) }}
+                  </div>
+                  <div :style='{marginLeft: "8px"}' class='cursor-pointer'>
+                    <q-icon name='content_copy' size='16px' class='text-grey-6' />
+                  </div>
+                </div>
+                <div class='text-grey-6'>
+                  {{ Number(chainBalance).toFixed(4) }}
+                </div>
               </div>
             </div>
-          </div>
-          <q-btn
-            flat rounded class='bg-red-2 full-width'
-            @click='onLogoutClick'
-            label='Logout'
-            :style='{margin: "24px 0 0 0"}'
-          />
-          <div class='text-grey-6 text-center' :style='{margin: "8px 0 16px 0"}'>
-            Powered by CheCko
-          </div>
-        </q-card>
-      </q-menu>
-      <q-img src='~assets/CheCko.png' width='24px' />
-      <div :style='{margin: "2px 0 0 8px"}' class='text-brown-8 text-bold'>
-        {{ account?.length ? shortid.shortId(account, 4) : 'Login' }}
+            <q-btn
+              flat rounded class='bg-red-2 full-width'
+              @click='onLogoutClick'
+              label='Logout'
+              :style='{margin: "24px 0 0 0"}'
+            />
+            <div class='text-grey-6 text-center' :style='{margin: "8px 0 16px 0"}'>
+              Powered by CheCko
+            </div>
+          </q-card>
+        </q-menu>
+        <q-img src='~assets/CheCko.png' width='24px' />
+        <div :style='{margin: "2px 0 0 8px"}' class='text-brown-8 text-bold'>
+          {{ account?.length ? shortid.shortId(account, 4) : 'Login' }}
+        </div>
+      </q-btn>
+      <div class='header-icon' v-if='account?.length'>
+        <q-icon
+          name='bi-grid-1x2' size='24px' :color='tab == "dashboard" ? "red-6" : "black"' class='cursor-pointer'
+          @click='onDashboardClick'
+        />
+        <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
+          Dashboard
+        </q-tooltip>
       </div>
-    </q-btn>
-    <div class='header-icon' v-if='account?.length'>
-      <q-icon
-        name='bi-grid-1x2' size='24px' :color='tab == "dashboard" ? "red-6" : "black"' class='cursor-pointer'
-        @click='onDashboardClick'
-      />
-      <q-tooltip :offset='[0, 4]' class='bg-grey-2 text-grey-8 shadow-4'>
-        Dashboard
-      </q-tooltip>
     </div>
   </div>
 </template>
