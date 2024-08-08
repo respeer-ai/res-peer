02647d8b2c853c9a0dae7fa211107cabbeabf0a8
Zhao KK <kikakkz@hotmail.com>
2024-08-08 09:07:59 +0000

    Support deploy

--- deploy-local.sh
+++ deploy-local.sh
@@ -107,15 +107,15 @@ print $'\U01f499' $LIGHTGREEN " CPRegistry application deployed"
 echo -e "    Bytecode ID:    $BLUE$cp_registry_bid$NC"
 echo -e "    Application ID: $BLUE$cp_registry_appid$NC"
 
+app_deploy_chain=`linera --with-wallet 1 wallet show | grep "Public Key" | awk '{print $2}'`
+app_deploy_owner=`linera --with-wallet 1 wallet show | grep "Owner" | awk '{print $4}'`
+
 print $'\U01F4AB' $YELLOW " Deploying Copilot application ..."
 copilot_bid=`linera --with-wallet 1 publish-bytecode ./target/wasm32-unknown-unknown/release/copilot_{contract,service}.wasm`
-copilot_appid=`linera --with-wallet 1 create-application $copilot_bid --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
+copilot_appid=`linera --with-wallet 1 create-application $copilot_bid --json-argument "{\"node_id\":\"d7a776b018fefbd45d533d3031c101bb64c29d52423beb6e4d5cf84e322ef429\",\"brand_logo\":\"https://ipfs.moralis.io:2053/ipfs/QmTDxnzcvj2p3xBrKcGv1wxoyhAn2yzCQnZZ9LmFjReuH9\",\"brand_name\":\"respeer.ai\",\"link_base\":\"http://172.16.31.73:9081\",\"resource_type\":\"CPU\",\"device_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"cpu_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"storage_type\":\"NVME\",\"storage_bytes\":100000000000,\"memory_bytes\":256000000000,\"free_quota\":10000,\"price_quota\":1000,\"quota_price\":\"0.001\",\"supported_task_types\":[\"FixGrammar\",\"RewriteEasierUnderstand\",\"Paraphrase\",\"WriteFormally\",\"WriteMoreNeutral\"],\"payment_chain_id\":\"$app_deploy_chain\"}" --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
 print $'\U01f499' $LIGHTGREEN " Copilot application deployed"
 echo -e "    Bytecode ID:    $BLUE$copilot_bid$NC"
 echo -e "    Application ID: $BLUE$copilot_appid$NC"
-
-app_deploy_chain=`linera --with-wallet 1 wallet show | grep "Public Key" | awk '{print $2}'`
-app_deploy_owner=`linera --with-wallet 1 wallet show | grep "Owner" | awk '{print $4}'`
 
 sed -i "s/feedApp =.*/feedApp = '$feed_appid',/g" webui/src/const/index.ts
 sed -i "s/creditApp =.*/creditApp = '$credit_appid',/g" webui/src/const/index.ts
@@ -126,6 +126,7 @@ sed -i "s/activityApp =.*/activityApp = '$activity_appid',/g" webui/src/const/in
 sed -i "s/blobGatewayApp =.*/blobGatewayApp = '$blob_gateway_appid',/g" webui/src/const/index.ts
 sed -i "s/cpRegistryApp =.*/cpRegistryApp = '$cp_registry_appid',/g" webui/src/const/index.ts
 sed -i "s/copilotApp =.*/copilotApp = '$copilot_appid'/g" webui/src/const/index.ts
+
 sed -i "s/export const appDeployChain =.*/export const appDeployChain = '$app_deploy_chain'/g" webui/src/const/index.ts
 sed -i "s/export const appDeployOwner =.*/export const appDeployOwner = '$app_deploy_owner'/g" webui/src/const/index.ts
 
