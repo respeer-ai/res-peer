e7ebf1c0f51c61d5228fc5eb6c2152127d9b24db
Zhao KK <kikakkz@hotmail.com>
2024-08-12 11:17:43 +0800

    Support query application states to avoid application query error

--- webui/src/components/RequestApplication.vue
+++ webui/src/components/RequestApplication.vue
@@ -3,12 +3,13 @@ import { onMounted, watch, computed } from 'vue'
 import gql from 'graphql-tag'
 import { getClientOptions } from 'src/apollo'
 import { ApolloClient } from '@apollo/client/core'
-import { provideApolloClient, useMutation } from '@vue/apollo-composable'
+import { provideApolloClient, useMutation, useQuery } from '@vue/apollo-composable'
 import * as constants from 'src/const'
 import { targetChain } from 'src/stores/chain'
 import { useApplicationStore } from 'src/stores/application'
 import { useUserStore } from 'src/stores/user'
 import { useSettingStore } from 'src/stores/setting'
+import { graphqlResult } from 'src/utils'
 
 const options = /* await */ getClientOptions(/* {app, router ...} */)
 const apolloClient = new ApolloClient(options)
@@ -22,6 +23,87 @@ const ready = () => {
   return (cheCkoConnect.value || targetChain.value?.length > 0) && account.value?.length > 0
 }
 
+const getApplications = (index: number, done?: (index: number, applicationIds: string[]) => void) => {
+  const { /* result, refetch, fetchMore, */ onResult /*, onError */ } = provideApolloClient(apolloClient)(() => useQuery(gql`
+    query getApplications ($chainId: String!) {
+      applications (chainId: $chainId) {
+        id
+      }
+    }
+  `, {
+    endpoint: 'main',
+    chainId: targetChain.value
+  }, {
+    fetchPolicy: 'network-only'
+  }))
+
+  onResult((res) => {
+    if (res.loading) return
+    const applications = graphqlResult.data(res, 'applications')
+    done?.(index, Array.from((applications as Record<string, string>[]).map((el) => el.id)))
+  })
+}
+
+const getApplicationsThroughCheCko = (index: number, done?: (index: number, applicationIds: string[]) => void) => {
+  const query = gql`
+    query getApplicationsThroughCheCko ($chainId: String!) {
+      applications (chainId: $chainId) {
+        id
+      }
+    }`
+  window.linera.request({
+    method: 'linera_graphqlQuery',
+    params: {
+      query: {
+        query: query.loc?.source?.body,
+        variables: {},
+        operationName: 'getApplicationsThroughCheCko'
+      }
+    }
+  }).then((result) => {
+    const applications = graphqlResult.keyValue(result, 'applications')
+    done?.(index, Array.from((applications as Record<string, string>[]).map((el) => el.id)))
+  }).catch((e) => {
+    console.log(e)
+  })
+}
+
+const onApplications = (index: number, applications: string[]) => {
+  if (applications.includes(constants.appIds[index])) {
+    switch (index) {
+      case 0:
+        application.feedApp = constants.Apps.feedApp
+        break
+      case 1:
+        application.creditApp = constants.Apps.creditApp
+        break
+      case 2:
+        application.marketApp = constants.Apps.marketApp
+        break
+      case 3:
+        application.reviewApp = constants.Apps.reviewApp
+        break
+      case 4:
+        application.activityApp = constants.Apps.activityApp
+        break
+      case 5:
+        application.foundationApp = constants.Apps.foundationApp
+        break
+      case 6:
+        application.blobGatewayApp = constants.Apps.blobGatewayApp
+        break
+      case 7:
+        application.cpRegistryApp = constants.Apps.cpRegistryApp
+        break
+      case 8:
+        application.copilotApp = constants.Apps.copilotApp
+        break
+    }
+  } else {
+    void requestApplication(index, true)
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const requestApplication = async (index: number, retry: boolean) => {
   if (index >= constants.appIds.length) {
@@ -30,7 +112,7 @@ const requestApplication = async (index: number, retry: boolean) => {
   if (retry) {
     setTimeout(() => {
       void requestApplication(index + 1, retry)
-    }, 3000)
+    }, 1000)
     return
   }
   const appId = constants.appIds[index]
@@ -40,38 +122,7 @@ const requestApplication = async (index: number, retry: boolean) => {
     }
   `))
   onDone(() => {
-    setTimeout(() => {
-      switch (index) {
-        case 0:
-          application.feedApp = constants.Apps.feedApp
-          break
-        case 1:
-          application.creditApp = constants.Apps.creditApp
-          break
-        case 2:
-          application.marketApp = constants.Apps.marketApp
-          break
-        case 3:
-          application.reviewApp = constants.Apps.reviewApp
-          break
-        case 4:
-          application.activityApp = constants.Apps.activityApp
-          break
-        case 5:
-          application.foundationApp = constants.Apps.foundationApp
-          break
-        case 6:
-          application.blobGatewayApp = constants.Apps.blobGatewayApp
-          break
-        case 7:
-          application.cpRegistryApp = constants.Apps.cpRegistryApp
-          break
-        case 8:
-          application.copilotApp = constants.Apps.copilotApp
-          break
-      }
-    }, 1000)
-    void requestApplication(index + 1, false)
+    getApplications(index, onApplications)
   })
   onError((error) => {
     void requestApplication(index, true)
@@ -85,6 +136,44 @@ const requestApplication = async (index: number, retry: boolean) => {
   })
 }
 
+const onApplicationsThroughCheCko = (index: number, applications: string[]) => {
+  if (applications.includes(constants.appIds[index])) {
+    console.log('Requested application', index, constants.appIds[index])
+    switch (index) {
+      case 0:
+        application.feedApp = constants.Apps.feedApp
+        break
+      case 1:
+        application.creditApp = constants.Apps.creditApp
+        break
+      case 2:
+        application.marketApp = constants.Apps.marketApp
+        break
+      case 3:
+        application.reviewApp = constants.Apps.reviewApp
+        break
+      case 4:
+        application.activityApp = constants.Apps.activityApp
+        break
+      case 5:
+        application.foundationApp = constants.Apps.foundationApp
+        break
+      case 6:
+        application.blobGatewayApp = constants.Apps.blobGatewayApp
+        break
+      case 7:
+        application.cpRegistryApp = constants.Apps.cpRegistryApp
+        break
+      case 8:
+        application.copilotApp = constants.Apps.copilotApp
+        break
+    }
+  } else {
+    console.log('ReRequest application', index, constants.appIds[index])
+    void requestApplicationThroughCheCko(index, true)
+  }
+}
+
 const requestApplicationThroughCheCko = (index: number, retry: boolean) => {
   if (index >= constants.appIds.length) {
     return
@@ -92,7 +181,7 @@ const requestApplicationThroughCheCko = (index: number, retry: boolean) => {
   if (retry) {
     setTimeout(() => {
       void requestApplicationThroughCheCko(index + 1, retry)
-    }, 3000)
+    }, 1000)
     return
   }
   const appId = constants.appIds[index]
@@ -113,38 +202,7 @@ const requestApplicationThroughCheCko = (index: number, retry: boolean) => {
       }
     }
   }).then(() => {
-    setTimeout(() => {
-      switch (index) {
-        case 0:
-          application.feedApp = constants.Apps.feedApp
-          break
-        case 1:
-          application.creditApp = constants.Apps.creditApp
-          break
-        case 2:
-          application.marketApp = constants.Apps.marketApp
-          break
-        case 3:
-          application.reviewApp = constants.Apps.reviewApp
-          break
-        case 4:
-          application.foundationApp = constants.Apps.foundationApp
-          break
-        case 5:
-          application.activityApp = constants.Apps.activityApp
-          break
-        case 6:
-          application.blobGatewayApp = constants.Apps.blobGatewayApp
-          break
-        case 7:
-          application.cpRegistryApp = constants.Apps.cpRegistryApp
-          break
-        case 8:
-          application.copilotApp = constants.Apps.copilotApp
-          break
-      }
-    }, 1000)
-    void requestApplicationThroughCheCko(index + 1, false)
+    getApplicationsThroughCheCko(index, onApplicationsThroughCheCko)
   }).catch((e) => {
     void requestApplicationThroughCheCko(index, true)
     console.log(e)
@@ -155,7 +213,10 @@ const requestApplications = () => {
   if (cheCkoConnect.value) {
     if (window.linera) {
       setTimeout(() => {
-        void requestApplicationThroughCheCko(0, false)
+        for (let i = 0; i < constants.appIds.length; i++) {
+          console.log('Request application', i, constants.appIds[i])
+          void requestApplicationThroughCheCko(i, false)
+        }
       }, 500)
     } else {
       setTimeout(() => {
@@ -163,7 +224,9 @@ const requestApplications = () => {
       }, 500)
     }
   } else {
-    void requestApplication(0, false)
+    for (let i = 0; i < constants.appIds.length; i++) {
+      void requestApplication(i, false)
+    }
   }
 }
 
