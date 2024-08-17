c25f3162c9fa79174683ab0e27be9efcd623f5be
Zhao KK <kikakkz@hotmail.com>
2024-08-17 16:12:38 +0800

    Safe exit

--- run_service.sh
+++ run_service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+killall -9 copilot-service illustrator-service
+
 cargo install --path service/copilot
 cargo install --path service/illustrator
 
@@ -8,4 +10,10 @@ AI_MODELS=$PROJECT_ROOT/ai-models
 COEDIT_T5_XXL=$AI_MODELS/coedit-t5/t5_xxl
 
 copilot-service --config-file $COEDIT_T5_XXL/config-xxl.json --weight-file $COEDIT_T5_XXL/model-xxl.gguf --tokenizer $COEDIT_T5_XXL/tokenizer.json > $PROJECT_ROOT/copilot-service.log 2>&1 &
-illustrator-service --height "720" --width "720" --sd-version "xl"  > $PROJECT_ROOT/illustrator-service.log 2>&1
+illustrator-service --height "720" --width "720" --sd-version "xl"  > $PROJECT_ROOT/illustrator-service.log 2>&1 &
+
+trap cleanup INT
+read -p "  Press any key to exit"
+print $'\U01f499' $LIGHTGREEN " Exit ..."
+
+killall -9 copilot-service illustrator-service
