dd9001d3362695397a5211c76cb4190fa372e6ea
Zhao KK <kikakkz@hotmail.com>
2024-10-21 06:31:07 +0000

    Support deploy to devnet

--- deploy-devnet.sh
+++ deploy-devnet.sh
@@ -1,41 +1,79 @@
 #!/bin/bash
 
-service_pids=`ps -ef | grep "linera " | grep -v "amount\|net" | awk '{print $2}'`
-kill -15 $service_pids > /dev/null 2>&1
+function cleanup() {
+  kill -15 `ps | grep linera-proxy | awk '{print $1}'` > /dev/null 2>&1
+  kill -15 `ps | grep linera-server | awk '{print $1}'` > /dev/null 2>&1
+  kill -15 `ps | grep linera | awk '{print $1}'` > /dev/null 2>&1
+  kill -15 `ps | grep socat | awk '{print $1}'` > /dev/null 2>&1
+}
+
+cleanup
 
 BLUE='\033[1;34m'
 YELLOW='\033[1;33m'
 LIGHTGREEN='\033[1;32m'
 NC='\033[0m'
 
+unset RUSTFLAGS
+unset TMPDIR
+unset ALL_PROXY
+unset all_proxy
+
 function print() {
   echo -e $1$2$3$NC
 }
 
-options="f:"
-faucet_url=https://faucet.devnet-2024-05-07.linera.net
+faucet_url=https://faucet.devnet-2024-09-04.linera.net
 
+options="f:"
 while getopts $options opt; do
   case ${opt} in
     f) faucet_url=${OPTARG} ;;
   esac
 done
 
-PROJECT_ROOT=/home/test
-NODE_LOG_FILE=$PROJECT_ROOT/linera-project/linera.log
-SERVICE_LOG_FILE=$PROJECT_ROOT/linera-project/service_8080.log
+PROJECT_ROOT=$HOME/linera-project
+
+NODE_LOG_FILE=$PROJECT_ROOT/linera.log
+SERVICE_LOG_FILE=$PROJECT_ROOT/service_8080.log
 WALLET_NUMBER=4
 EXTRA_WALLET_NUMBER=`expr $WALLET_NUMBER - 1`
-WALLET_BASEDIR=$PROJECT_ROOT/.config/linera
 
-print $'\U01F4AB' $YELLOW " Running lienra net, log in $NODE_LOG_FILE ..."
+WALLET_BASE=$PROJECT_ROOT/linera/respeer
+mkdir -p $WALLET_BASE
+rm -rf $WALLET_BASE/*
+
+print $'\U01F4AB' $YELLOW " Running linera net, log in $NODE_LOG_FILE ..."
+
+function create_wallet() {
+  export LINERA_WALLET_$1=$WALLET_BASE/wallet_$1.json
+  export LINERA_STORAGE_$1=rocksdb:$WALLET_BASE/client_$1.db
+
+  linera -w $1 wallet init --faucet $faucet_url --with-new-chain
+  linera -w $1 wallet show
+}
 
 for i in `seq 0 $EXTRA_WALLET_NUMBER`; do
-  print $'\U01f499' $LIGHTGREEN " Initialize wallet $i"
-  export LINERA_WALLET_$i="$WALLET_BASEDIR/wallet_$i.json"
-  export LINERA_STORAGE_$i="rocksdb:$WALLET_BASEDIR/wallet_$i.db"
-  linera --with-wallet $i wallet init --with-new-chain --faucet $faucet_url
+  create_wallet $i
 done
+
+function __run_service() {
+  linera -w $1 service --port $2 --external-signing false
+  if [ ! $? -eq 0 ]; then
+    echo "Run with official release"
+    linera -w $1 service --port $2
+  fi
+}
+
+function run_service () {
+  local_port=`expr 9080 + $1`
+  pub_port=`expr 10100 + $1`
+
+  __run_service $1 $local_port > $PROJECT_ROOT/service_$local_port.log 2>&1 &
+
+  sleep 3
+  socat TCP4-LISTEN:$pub_port TCP4:localhost:$local_port
+}
 
 print $'\U01F4AB' $YELLOW " Deploying Credit bytecode ..."
 credit_bid=`linera --with-wallet 0 publish-bytecode ./target/wasm32-unknown-unknown/release/credit_{contract,service}.wasm`
@@ -96,7 +134,7 @@ echo -e "    Application ID: $BLUE$cp_registry_appid$NC"
 
 print $'\U01F4AB' $YELLOW " Deploying Copilot CPU application ..."
 copilot_cpu_bid=`linera --with-wallet 0 publish-bytecode ./target/wasm32-unknown-unknown/release/copilot_{contract,service}.wasm`
-copilot_cpu_appid=`linera --with-wallet 0 create-application $copilot_cpu_bid --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
+copilot_cpu_appid=`linera --with-wallet 1 create-application $copilot_cpu_bid --json-argument "{\"node_id\":\"d7a776b018fefbd45d533d3031c101bb64c29d52423beb6e4d5cf84e322ef429\",\"brand_logo\":\"https://github.com/respeer-ai/res-peer/blob/master/webui/public/favicon.png?raw=true\",\"brand_name\":\"respeer.ai\",\"link_base\":\"http://172.16.31.73:9081\",\"resource_type\":\"CPU\",\"device_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"cpu_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"storage_type\":\"NVME\",\"storage_bytes\":100000000000,\"memory_bytes\":256000000000,\"free_quota\":3,\"price_quota\":1,\"quota_price\":\"0.003\",\"supported_task_types\":[\"FixGrammar\",\"RewriteEasierUnderstand\",\"Paraphrase\",\"WriteFormally\",\"WriteMoreNeutral\"],\"payment_chain_id\":\"$app_deploy_chain\",\"ai_model\":\"CoEDiT T5\",\"ai_model_url\":\"https://huggingface.co/jbochi/candle-coedit-quantized\"}" --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
 print $'\U01f499' $LIGHTGREEN " Copilot CPU application deployed"
 echo -e "    Bytecode ID:    $BLUE$copilot_cpu_bid$NC"
 echo -e "    Application ID: $BLUE$copilot_cpu_appid$NC"
@@ -104,14 +142,14 @@ echo -e "    Application ID: $BLUE$copilot_cpu_appid$NC"
 print $'\U01F4AB' $YELLOW " Deploying Copilot GPU application ..."
 copilot_fetch_server_url="http://localhost:9071/?prompt="
 copilot_gpu_bid=`linera --with-wallet 0 publish-bytecode ./target/wasm32-unknown-unknown/release/copilot_{contract,service}.wasm`
-copilot_gpu_appid=`linera --with-wallet 0 create-application $copilot_gpu_bid --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
+copilot_gpu_appid=`linera --with-wallet 1 create-application $copilot_gpu_bid --json-argument "{\"node_id\":\"d7a776b018fefbd45d533d3031c101bb64c29d52423beb6e4d5cf84e322ef429\",\"brand_logo\":\"https://github.com/respeer-ai/res-peer/blob/master/webui/public/favicon.png?raw=true\",\"brand_name\":\"respeer.ai\",\"link_base\":\"http://172.16.31.73:9081\",\"resource_type\":\"GPU\",\"device_model\":\"NVIDIA GeForce RTX 3090\",\"cpu_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"storage_type\":\"NVME\",\"storage_bytes\":100000000000,\"memory_bytes\":256000000000,\"free_quota\":3,\"price_quota\":1,\"quota_price\":\"0.003\",\"supported_task_types\":[\"FixGrammar\",\"RewriteEasierUnderstand\",\"Paraphrase\",\"WriteFormally\",\"WriteMoreNeutral\"],\"payment_chain_id\":\"$app_deploy_chain\",\"fetch_server_url\":\"$copilot_fetch_server_url\",\"ai_model\":\"CoEDiT T5\",\"ai_model_url\":\"https://huggingface.co/jbochi/candle-coedit-quantized\"}" --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
 print $'\U01f499' $LIGHTGREEN " Copilot GPU application deployed"
 echo -e "    Bytecode ID:    $BLUE$copilot_gpu_bid$NC"
 echo -e "    Application ID: $BLUE$copilot_gpu_appid$NC"
 
 print $'\U01F4AB' $YELLOW " Deploying Illustrator CPU application ..."
 illustrator_cpu_bid=`linera --with-wallet 0 publish-bytecode ./target/wasm32-unknown-unknown/release/illustrator_{contract,service}.wasm`
-illustrator_cpu_appid=`linera --with-wallet 0 create-application $illustrator_cpu_bid --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
+illustrator_cpu_appid=`linera --with-wallet 1 create-application $illustrator_cpu_bid --json-argument "{\"node_id\":\"d7a776b018fefbd45d533d3031c101bb64c29d52423beb6e4d5cf84e322ef429\",\"brand_logo\":\"https://github.com/respeer-ai/res-peer/blob/master/webui/public/favicon.png?raw=true\",\"brand_name\":\"respeer.ai\",\"link_base\":\"http://172.16.31.73:9081\",\"resource_type\":\"CPU\",\"device_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"cpu_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"storage_type\":\"NVME\",\"storage_bytes\":100000000000,\"memory_bytes\":256000000000,\"free_quota\":3,\"price_quota\":1,\"quota_price\":\"0.03\",\"supported_task_types\":[\"GenerateIllustrate\"],\"payment_chain_id\":\"$app_deploy_chain\",\"ai_model\":\"Tiny Stable Diffusion\",\"ai_model_url\":\"https://huggingface.co/segmind/tiny-sd\"}" --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
 print $'\U01f499' $LIGHTGREEN " Illustrator CPU application deployed"
 echo -e "    Bytecode ID:    $BLUE$illustrator_cpu_bid$NC"
 echo -e "    Application ID: $BLUE$illustrator_cpu_appid$NC"
@@ -119,7 +157,7 @@ echo -e "    Application ID: $BLUE$illustrator_cpu_appid$NC"
 print $'\U01F4AB' $YELLOW " Deploying Illustrator GPU application ..."
 illustrator_fetch_server_url="http://localhost:9072/?prompt="
 illustrator_gpu_bid=`linera --with-wallet 0 publish-bytecode ./target/wasm32-unknown-unknown/release/illustrator_{contract,service}.wasm`
-illustrator_gpu_appid=`linera --with-wallet 0 create-application $illustrator_gpu_bid --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
+illustrator_gpu_appid=`linera --with-wallet 1 create-application $illustrator_gpu_bid --json-argument "{\"node_id\":\"d7a776b018fefbd45d533d3031c101bb64c29d52423beb6e4d5cf84e322ef429\",\"brand_logo\":\"https://github.com/respeer-ai/res-peer/blob/master/webui/public/favicon.png?raw=true\",\"brand_name\":\"respeer.ai\",\"link_base\":\"http://172.16.31.73:9081\",\"resource_type\":\"GPU\",\"device_model\":\"NVIDIA GeForce RTX 3090\",\"cpu_model\":\"Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz\",\"storage_type\":\"NVME\",\"storage_bytes\":100000000000,\"memory_bytes\":256000000000,\"free_quota\":3,\"price_quota\":1,\"quota_price\":\"0.03\",\"supported_task_types\":[\"GenerateIllustrate\"],\"payment_chain_id\":\"$app_deploy_chain\",\"fetch_server_url\":\"$illustrator_fetch_server_url\",\"ai_model\":\"Tiny Stable Diffusion\",\"ai_model_url\":\"https://huggingface.co/segmind/tiny-sd\"}" --json-parameters "{\"cp_registry_app_id\":\"$cp_registry_appid\"}" --required-application-ids $cp_registry_appid`
 print $'\U01f499' $LIGHTGREEN " Illustrator GPU application deployed"
 echo -e "    Bytecode ID:    $BLUE$illustrator_gpu_bid$NC"
 echo -e "    Application ID: $BLUE$illustrator_gpu_appid$NC"
@@ -143,24 +181,9 @@ sed -i "s/illustratorGpuApp =.*/illustratorGpuApp = '$illustrator_gpu_appid'/g" 
 sed -i "s/export const appDeployChain =.*/export const appDeployChain = '$app_deploy_chain'/g" webui/src/const/index.ts
 sed -i "s/export const appDeployOwner =.*/export const appDeployOwner = '$app_deploy_owner'/g" webui/src/const/index.ts
 
-function run_new_service() {
-  BASE_PORT=9080
-  port=`expr $BASE_PORT + $1`
-  print $'\U01f499' $LIGHTGREEN " Wallet of $port service ..."
-  linera --with-wallet $1 wallet show
-  print $'\U01f499' $LIGHTGREEN " Run $port service ..."
-  LOG_FILE=`echo $SERVICE_LOG_FILE | sed "s/8080/$port/g"`
-  linera --with-wallet $1 service --external-signing false --port $port > $LOG_FILE 2>&1 &
-}
-
 for i in `seq 0 $EXTRA_WALLET_NUMBER`; do
-  run_new_service $i
+  run_service $i
 done
-
-function cleanup() {
-  service_pids=`ps -ef | grep "linera " | grep -v "amount\|net" | awk '{print $2}'`
-  kill -15 $service_pids > /dev/null 2>&1
-}
 
 trap cleanup INT
 read -p "  Press any key to exit"
